deb479d91ba9eccdbfda27870a3c0700cb928aa7
shimsihim <122508398+shimsihim@users.noreply.github.com>
2023-11-06 21:29:39 +0900

    [level 2] Title: 스킬트리, Time: 0.13 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -11,11 +11,13 @@ class Solution {
         skills = skill_trees;
         map = new HashMap<>();
         
-        //일단 해시맵에 order를 저장
+        //일단 해시맵에 order를 저장 각각의 문자 순서대로 1,2,3,부과
         saveorder();
         
+        
         int res = 0;
-        //각 문자열을 순회하며 
+        
+        //각 문자열을 순회하며  해당 스킬트리가 맞으면 res++
         for(int i = 0 ; i <skill_trees.length;++i ){
             if(find(skill_trees[i])){
                 res++;
@@ -28,10 +30,14 @@ class Solution {
     
     static boolean find(String str){
         
+        //선행스킬을 1,2,3의 순서이므로 가장 초반에는 0
         int preindex = 0;
         
+        //문자열을 순회하며
         for(int i = 0 ; i < str.length();++i){
+            // 만약 map에 저장된 문자열이라면 선행스킬세트에 포함된 것
             if(map.containsKey(str.charAt(i))){
+                //이번에 나온 스킬이 선행스킬 뒤에 나온것이 아니라면 false
                 if(map.get(str.charAt(i))!=preindex+1){
                     return false;
                 }
