db57d949ab3453da99c52724972fbf55bc9128c7
shimsihim <122508398+shimsihim@users.noreply.github.com>
2023-11-28 13:35:18 +0900

    [level 2] Title: 땅따먹기, Time: 15.24 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,26 +1,10 @@
 class Solution {
-    static int[][] lands;
     int solution(int[][] land) {
-        lands = land;
-        
-        for(int i = 1 ; i < land.length; ++i){
-            for(int j = 0 ; j < 4; ++j){
-                find(i,j);
-            }
-        }
-        int max = 0;
-        for(int i = 0 ; i < 4 ; ++i){
-            max = Math.max(lands[lands.length-1][i],max);
-        }
-        return max;
-    }
-    
-    static void find(int r , int c){
-        int max = 0;
-        for(int i = 0 ; i < 4 ; ++i){
-            if(i==c) continue;
-            max = Math.max(max,lands[r-1][i]);
-        }
-        lands[r][c] = max + lands[r][c];
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
     }
 }
