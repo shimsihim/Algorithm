ba416acf075d4aa7a6b4b78eb7b71440b179646a
shimsihim <122508398+shimsihim@users.noreply.github.com>
2025-10-24 14:49:40 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 21.56 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 쿼드압축 후 개수 세기 - 68936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936?language=java) 
 
 ### 성능 요약
 
-메모리: 93.6 MB, 시간: 11.63 ms
+메모리: 119 MB, 시간: 21.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 9:7:47
+2025년 10월 24일 14:49:36
 
 ### 문제 설명
 
--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,76 +1,44 @@
 class Solution {
-    public int[] solution(int[][] arr) {
-        
-        // 구간별로 메서드로 보내기,//해당 공간이 모두 같은 것으로 채워졌을 때 반환,  반환 값은 해당 구간 내에 0과 1의 개수 반환하기
-        int n = arr.length;
-        
-        
-        int[] answer = check(0,0,n-1,n-1 , arr);
-        
-     
-        
-        
-        
-        return answer;
+    public int[] solution(int[][] arr){
+        this.map = arr;
+        return getArr(0,0,arr.length);
     }
-    
-    static int[] check(int startR,int startC, int endR, int endC,int[][] arr){
-        
-        //일단 checkCnt로 보내서 개수 확인
-        
-        int temp[] = checkCnt(startR,startC,endR,endC,arr);
-        // 둘다 0ㅣ 아니면 쪼개서 check호출
-        
-        int answer[] = new int[2];
-        if(temp[0]!=0 && temp[1]!=0){ // 둘 다 0이 아닌경우 즉, 섞여있는 경우
-            int size = (endR-startR+1)/2 -1;
-            temp = check(startR,startC,startR+size,startC+size,arr);
-            answer[0] += temp[0];
-            answer[1] += temp[1];
-            temp = check(endR-size,endC-size,endR,endC,arr);
-            answer[0] += temp[0];
-            answer[1] += temp[1];
-            temp = check(startR,endC-size,startR+size,endC,arr);
-            answer[0] += temp[0];
-            answer[1] += temp[1];
-            temp = check(endR-size,startC,endR,startC+size,arr);
-            answer[0] += temp[0];
-            answer[1] += temp[1];
-            
-            return answer;
-        }
-        
-        //둘 중 1개가 0이면 return 해주기
-        
-        else{
-            if(temp[0] == 0){
-                return new int[] {0,1};
-            }
-            else{
-                return new int[] {1,0};
-            }
-        }
-        
-        
-        
-    }
-    
-    
-    static int[] checkCnt(int startR,int startC, int endR, int endC,int[][] arr){
-        
-        int ret[] = new int[2];
-        
-        for(int i = startR ; i <= endR ; ++i ){
-            for(int j = startC ; j <= endC ; ++j){
-                if(arr[i][j]==0){
-                    ret[0]++;
-                }else{
-                    ret[1]++;
+
+    static int[][] map;
+
+    int[] getArr(int startR , int startC ,int length){
+
+        if(length == 1) return toArr(map[startR][startC]);
+
+        int nowVal = map[startR][startC];
+        boolean isSuccessFlag = true;
+        all : for (int i = startR; i < startR + length ; i++) {
+            for (int j = startC; j < startC + length; j++) {
+                if (nowVal != map[i][j]) {
+                    isSuccessFlag = false;
+                    break all;
                 }
             }
         }
-        
-        return ret;
-        
+        if(isSuccessFlag){
+            return toArr(nowVal);
+        }
+
+
+        int[] ans1 = getArr(startR , startC , length/2);
+        int[] ans2 = getArr(startR + length/2 , startC , length/2);
+        int[] ans3 = getArr(startR , startC + length/2 , length/2);
+        int[] ans4 = getArr(startR + length/2 , startC + length/2 , length/2);
+
+        ans1[0] += ans2[0] + ans3[0] + ans4[0];
+        ans1[1] += ans2[1] + ans3[1] + ans4[1];
+        return ans1;
+    }
+
+    int[] toArr(int val){
+        if(val == 0){
+            return new int[] {1,0};
+        }
+        return new int[] {0,1};
     }
 }
